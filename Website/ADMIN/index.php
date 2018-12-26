3d9ddd06f63c213c1d505762f7911f3a1618a182
2163054 <37394468+2163054@users.noreply.github.com>
2018-12-26 10:06:39 +0800

    Initial UI for admin
    +Maybe modified for improvement.

---
+++ Website/ADMIN/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php include('header.php'); ?>
+<body>
+<div class="container" style="margin-left: 10%; margin-right: 10%;">
+	<table class="table table-hover">
+		<tbody>
+			<tr>
+				<td>New accounts registered</td>
+				<td>15</td>
+				<td><button>View</button></td>
+			</tr>
+			<tr>
+				
+			</tr>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
